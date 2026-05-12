b770584642031623559319717c37a53b515e4611
Pancake <yidong.zhu@hannto.com>
2026-05-12 18:14:37 +0800

    添加一部分用于接口测试的断言

---
+++ apitest/core/base.py
@@ -0,0 +1,149 @@
+from typing import \
+  Union as _Union, \
+  List as _List,   \
+  Tuple as _Tuple, \
+  overload as _overload
+from unittest.util import safe_repr as _safe_repr
+from unittest import TestCase as _TestCase
+
+
+class Base(_TestCase):
+  """接口测试用例基类"""
+
+  @staticmethod
+  def _get_status_prefix(status_type: str) -> int | None:
+    """
+    获取状态码的前缀
+
+    Args:
+      status_type: 状态类型
+
+    Returns:
+      状态码前缀
+
+    """
+    match status_type:
+      case 'informational':
+        return 100
+      case 'success':
+        return 200
+      case 'redirection':
+        return 300
+      case 'client_error':
+        return 400
+      case 'server_error':
+        return 500
+      case _:
+        return None
+
+  def assertStatusCodeEqual(self, status_code: int, status_code_expected: int, msg=None) -> None:
+    """断言。如果响应状态码不等于预期，则失败。"""
+    if not status_code == status_code_expected:
+      msg = self._formatMessage(msg, '响应状态码 %s 不等于 %s' % (
+        _safe_repr(status_code),
+        _safe_repr(status_code_expected)
+      ))
+      raise self.failureException(msg)
+
+  def assertStatusCodeNotEqual(self, status_code: int, status_codes_expected: int, msg=None) -> None:
+    """断言。如果响应状态码等于预期，则失败。"""
+    if not status_code != status_codes_expected:
+      msg = self._formatMessage(msg, '响应状态码 %s 等于 %s' % (
+        _safe_repr(status_code),
+        _safe_repr(status_codes_expected)
+      ))
+      raise self.failureException(msg)
+
+  def assertStatusCodeIn(
+    self,
+    status_code: int,
+    status_codes_expected: _Union[_List[int], _Tuple[int]],
+    msg = None
+  ):
+    """断言。如果响应状态码不在预期的容器中，则失败。"""
+    if status_code not in status_codes_expected:
+      standard_msg = '响应状态码 %s 不在 %s 中' % (
+        _safe_repr(status_code),
+        _safe_repr(status_codes_expected)
+      )
+      self.fail(self._formatMessage(msg, standard_msg))
+
+  def assertStatusCodeNotIn(
+    self,
+    status_code: int,
+    status_codes_expected: _Union[_List[int], _Tuple[int]],
+    msg = None
+  ):
+    """断言。如果响应状态码在预期的容器中，则失败。"""
+    if status_code in status_codes_expected:
+      standard_msg = '响应状态码 %s 在 %s 中' % (
+        _safe_repr(status_code),
+        _safe_repr(status_codes_expected)
+      )
+      self.fail(self._formatMessage(msg, standard_msg))
+
+  @_overload
+  def assertStatusTypeIs(self, status_code: int, status_type_expected: str, msg = None): ...
+
+  @_overload
+  def assertStatusTypeIs(self, status_code: int, status_type_expected: int, msg = None): ...
+
+  def assertStatusTypeIs(self, status_code: int, status_type_expected, msg = None):
+    """断言。如果响应状态码不是预期的状态类型，则失败。"""
+
+    status_prefix: int
+
+    # 整数
+    if isinstance(status_type_expected, int):
+      # 状态码前缀
+      status_prefix = (status_type_expected // 100) * 100
+    # 字符串
+    elif isinstance(status_type_expected, str):
+      # 状态码前缀
+      status_prefix = self._get_status_prefix(status_type_expected)
+    # 错误
+    else:
+      raise TypeError('status must be int or str')
+
+    a = status_code - status_prefix
+    if not 0 <= a < 100:
+      standard_msg = '响应状态码 %s 不属于响应状态 %s' % (
+        _safe_repr(status_code),
+        _safe_repr(status_type_expected)
+      )
+      self.fail(self._formatMessage(msg, standard_msg))
+
+
+  @_overload
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: str, msg=None): ...
+
+  @_overload
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: int, msg=None): ...
+
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected, msg=None):
+    """断言。如果响应状态码是预期的状态类型，则失败。"""
+
+    status_prefix: int
+
+    # 整数
+    if isinstance(status_type_expected, int):
+      # 状态码前缀
+      status_prefix = (status_type_expected // 100) * 100
+    # 字符串
+    elif isinstance(status_type_expected, str):
+      # 状态码前缀
+      status_prefix = self._get_status_prefix(status_type_expected)
+    # 错误
+    else:
+      raise TypeError('status must be int or str')
+
+    a = status_code - status_prefix
+    if 0 <= a < 100:
+      standard_msg = '响应状态码 %s 属于响应状态 %s' % (
+        _safe_repr(status_code),
+        _safe_repr(status_type_expected)
+      )
+      self.fail(self._formatMessage(msg, standard_msg))
+
+  def assertStatusTypeIn(self, status_code: int, status_types_expected: int, msg=None): ...
+
--- apitest/core/case.py
+++ apitest/core/case.py
@@ -1,20 +1,68 @@
-from unittest import TestCase as _TestCase
-from unittest import TestLoader as _TestLoader
 import requests as _requests
 import ddt as _ddt
+from unittest import TestLoader as _TestLoader
+from apitest.core.base import Base as _Base
 
 _TestLoader.testMethodPrefix = 'api_test'
 
 
 @_ddt.ddt
-class TestCase(_TestCase):
+class TestCase(_Base):
   @_ddt.data(
-    {'url': 'http://localhost/', 'method': 'GET'},
-    {'url': 'http://localhost/', 'method': 'POST'}
+    {
+      'url': 'https://www.baidu.com/',
+      'method': 'GET',
+      'assertions': {
+        'status': {
+          'is': 200,
+          'is_not': [301, 302],
+          'type_is': 'informational',
+          # informational: 响应状态码100+
+          # success: 响应状态码200+
+          # redirection: 响应状态码300+
+          # client_error: 响应状态码400+
+          # server_error: 响应状态码500+
+          'type_is_not': ['informational', 'success', 'redirection', 'client_error', 'server_error'],
+        },
+        'headers':{
+          'content-type': 'application/json',
+        },
+        'content':{
+          'json'
+        }
+      }
+    },
+    {'url': 'https://www.baidu.com/', 'method': 'POST', 'assertions': {'IS_NOT': 200}},
   )
   @_ddt.unpack
-  def api_test(self, method: str, url: str, **kwargs):
+  def api_test(self, method: str, url: str, assertions: dict, **kwargs):
     response = _requests.request(method = method.upper(), url = url, **kwargs)
+
+    # 响应状态码断言
+
+    if 'status' in assertions:
+
+      # 获取断言信息
+      code_assertions = assertions.get('status')
+
+      # 断言响应状态码是...
+      if 'is' in code_assertions:
+        self.assertEqual(response.status_code, code_assertions.get('is'))
+
+      # 断言响应状态码不是...
+      elif 'is' in code_assertions:
+        ...
+
+      # 断言响应状态码前缀是...
+      if 'like' in code_assertions:
+        ...
+
+      # 断言响应状态码前缀不是...
+      elif 'not_like' in code_assertions:
+        ...
+
+
+    # 响应内容断言
 
     ...
 
