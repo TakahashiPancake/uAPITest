db73ee3ede53b80dec0354504fab2a0f155f5c0c
Pancake <yidong.zhu@hannto.com>
2026-05-12 19:43:37 +0800

    添加对响应状态类型的断言

--- apitest/core/base.py
+++ apitest/core/base.py
@@ -36,7 +36,45 @@ class Base(_TestCase):
       case _:
         return None
 
-  def assertStatusCodeEqual(self, status_code: int, status_code_expected: int, msg=None) -> None:
+  def _assertStatusTypeBase(
+    self,
+    status_code: int,
+    status_type_expected,
+    msg = None,
+    assert_is: bool = False
+  ) -> None:
+    status_prefix: int
+
+    # 整数
+    if isinstance(status_type_expected, int):
+      # 状态码前缀
+      status_prefix = (status_type_expected // 100) * 100
+    # 字符串
+    elif isinstance(status_type_expected, str):
+      # 状态码前缀
+      status_prefix = self._get_status_prefix(status_type_expected)
+    # 错误
+    else:
+      raise TypeError('status must be int or str')
+
+    a = status_code - status_prefix
+
+    if assert_is:
+      if not 0 <= a < 100:
+        standard_msg = '响应状态码 %s 不属于响应状态 %s' % (
+          _safe_repr(status_code),
+          _safe_repr(status_type_expected)
+        )
+        self.fail(self._formatMessage(msg, standard_msg))
+    else:
+      if 0 <= a < 100:
+        standard_msg = '响应状态码 %s 属于响应状态 %s' % (
+          _safe_repr(status_code),
+          _safe_repr(status_type_expected)
+        )
+        self.fail(self._formatMessage(msg, standard_msg))
+
+  def assertStatusCodeEqual(self, status_code: int, status_code_expected: int, msg = None) -> None:
     """断言。如果响应状态码不等于预期，则失败。"""
     if not status_code == status_code_expected:
       msg = self._formatMessage(msg, '响应状态码 %s 不等于 %s' % (
@@ -45,7 +83,7 @@ class Base(_TestCase):
       ))
       raise self.failureException(msg)
 
-  def assertStatusCodeNotEqual(self, status_code: int, status_codes_expected: int, msg=None) -> None:
+  def assertStatusCodeNotEqual(self, status_code: int, status_codes_expected: int, msg = None) -> None:
     """断言。如果响应状态码等于预期，则失败。"""
     if not status_code != status_codes_expected:
       msg = self._formatMessage(msg, '响应状态码 %s 等于 %s' % (
@@ -90,60 +128,33 @@ class Base(_TestCase):
 
   def assertStatusTypeIs(self, status_code: int, status_type_expected, msg = None):
     """断言。如果响应状态码不是预期的状态类型，则失败。"""
-
-    status_prefix: int
-
-    # 整数
-    if isinstance(status_type_expected, int):
-      # 状态码前缀
-      status_prefix = (status_type_expected // 100) * 100
-    # 字符串
-    elif isinstance(status_type_expected, str):
-      # 状态码前缀
-      status_prefix = self._get_status_prefix(status_type_expected)
-    # 错误
-    else:
-      raise TypeError('status must be int or str')
-
-    a = status_code - status_prefix
-    if not 0 <= a < 100:
-      standard_msg = '响应状态码 %s 不属于响应状态 %s' % (
-        _safe_repr(status_code),
-        _safe_repr(status_type_expected)
-      )
-      self.fail(self._formatMessage(msg, standard_msg))
-
+    self._assertStatusTypeBase(status_code, status_type_expected, msg = msg, assert_is = True)
 
   @_overload
-  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: str, msg=None): ...
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: str, msg = None): ...
 
   @_overload
-  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: int, msg=None): ...
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected: int, msg = None): ...
 
-  def assertStatusTypeIsNot(self, status_code: int, status_type_expected, msg=None):
+  def assertStatusTypeIsNot(self, status_code: int, status_type_expected, msg = None):
     """断言。如果响应状态码是预期的状态类型，则失败。"""
+    self._assertStatusTypeBase(status_code, status_type_expected, msg = msg, assert_is = False)
 
-    status_prefix: int
+  def assertStatusTypeIn(
+    self,
+    status_code: int,
+    status_types_expected: _Union[_List[_Union[int, str]], _Tuple[_Union[int, str]]],
+    msg = None
+  ):
+    for status_type_expected in status_types_expected:
+      self._assertStatusTypeBase(status_code, status_type_expected, msg = msg, assert_is = True)
 
-    # 整数
-    if isinstance(status_type_expected, int):
-      # 状态码前缀
-      status_prefix = (status_type_expected // 100) * 100
-    # 字符串
-    elif isinstance(status_type_expected, str):
-      # 状态码前缀
-      status_prefix = self._get_status_prefix(status_type_expected)
-    # 错误
-    else:
-      raise TypeError('status must be int or str')
-
-    a = status_code - status_prefix
-    if 0 <= a < 100:
-      standard_msg = '响应状态码 %s 属于响应状态 %s' % (
-        _safe_repr(status_code),
-        _safe_repr(status_type_expected)
-      )
-      self.fail(self._formatMessage(msg, standard_msg))
-
-  def assertStatusTypeIn(self, status_code: int, status_types_expected: int, msg=None): ...
+  def assertStatusTypeNotIn(
+    self,
+    status_code: int,
+    status_types_expected: _Union[_List[_Union[int, str]], _Tuple[_Union[int, str]]],
+    msg = None
+  ):
+    for status_type_expected in status_types_expected:
+      self._assertStatusTypeBase(status_code, status_type_expected, msg = msg, assert_is = False)
 
